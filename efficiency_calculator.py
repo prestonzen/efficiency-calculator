3c385d73247ce90b10e4ef132d363f93035a4690
Preston Zen <36439263+prestonzen@users.noreply.github.com>
2020-03-26 22:53:36 -0700

    Update efficiency_calculator.py
    Instructions for google calendar intergration

--- efficiency_calculator.py
+++ efficiency_calculator.py
@@ -5,7 +5,6 @@ Week_Hours = 168
 
 List of tasks to complete that week
 
-
 [Task_List] = input("Enter tasks to complete this week separated by , ")
 
 #For loop?
@@ -13,10 +12,11 @@ List of tasks to complete that week
 Daily_Sleep = input("How many hours a night do you sleep?")
 Weekly_Sleep = Daily_Sleep * 7
 
-
 #working hours per week
-
 #Task_Time = (Week_Hours - Weekly_Sleep) / count([Task_List])
 
 print("You have " + Task_Time + " hours to do each task")
 print("Your weekly tasks are: " + [Task_List] ) 
+
+#google calendar intergration
+#https://developers.google.com/calendar/quickstart/python
