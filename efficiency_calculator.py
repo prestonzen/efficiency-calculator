6bd4f073a27048644635f37a21585ccdf20271de
Preston Zen <36439263+prestonzen@users.noreply.github.com>
2020-03-26 22:49:02 -0700

    Create efficiency_calculator.py
    Basic brain dump of code

---
+++ efficiency_calculator.py
@@ -0,0 +1,22 @@
+#Written by Preston Zen and Jacob Mosses
+
+Work efficiency calculator
+Week_Hours = 168
+
+List of tasks to complete that week
+
+
+[Task_List] = input("Enter tasks to complete this week separated by , ")
+
+#For loop?
+
+Daily_Sleep = input("How many hours a night do you sleep?")
+Weekly_Sleep = Daily_Sleep * 7
+
+
+#working hours per week
+
+#Task_Time = (Week_Hours - Weekly_Sleep) / count([Task_List])
+
+print("You have " + Task_Time + " hours to do each task")
+print("Your weekly tasks are: " + [Task_List] ) 
